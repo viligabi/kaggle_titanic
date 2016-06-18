5afcb4164de293cba064efc9201da98f408c4388
viligabi <viligabi@gmail.com>
2016-06-18 15:09:49 +0200

    Add: optimize_data() Fix: more features are used for prediction

--- titanic.py
+++ titanic.py
@@ -16,22 +16,51 @@ def accuracy_score(truth, pred):
     else:
         return "Number of predictions does not match number of outcomes!"
 
-
+def optimize_data(df_):
+    df = df_.copy()
+    try:
+        df.drop(['Ticket'], inplace=True, axis=1)
+    except: pass
+    try:
+        df.drop(['Name'], inplace=True, axis=1)
+    except:
+        pass
+    try:
+        df.drop(["PassengerId"], inplace=True, axis=1)
+    except:
+        pass
+    try:
+        df.drop(["Survived"], inplace=True, axis=1)
+    except:
+        pass
+    df.loc[df.Sex != 'male', 'Sex'] = 0
+    df.loc[df.Sex == 'male', 'Sex'] = 1
+    df.Age = df.Age.fillna(df.Age.mean())
+    df.Cabin.fillna('0', inplace=True)
+    df.loc[df.Cabin.str[0] == 'A', 'Cabin'] = 1
+    df.loc[df.Cabin.str[0] == 'B', 'Cabin'] = 2
+    df.loc[df.Cabin.str[0] == 'C', 'Cabin'] = 3
+    df.loc[df.Cabin.str[0] == 'D', 'Cabin'] = 4
+    df.loc[df.Cabin.str[0] == 'E', 'Cabin'] = 5
+    df.loc[df.Cabin.str[0] == 'F', 'Cabin'] = 6
+    df.loc[df.Cabin.str[0] == 'G', 'Cabin'] = 7
+    df.loc[df.Cabin.str[0] == 'T', 'Cabin'] = 8
+    df.Embarked.fillna(0, inplace=True)
+    df.loc[df.Embarked == 'C', 'Embarked'] = 1
+    df.loc[df.Embarked == 'Q', 'Embarked'] = 2
+    df.loc[df.Embarked == 'S', 'Embarked'] = 3
+    df.fillna(-1, inplace=True)
+    return df.astype(float)
 # Import train and test raw data
 train_data_raw = pd.read_csv("train.csv")
 test_data_raw = pd.read_csv("test.csv")
 
-train_data = train_data_raw[["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare"]]
+train_data = optimize_data(train_data_raw)
+test_data = optimize_data(test_data_raw)
+#train_data = train_data_raw[["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare"]]
 train_data_results = train_data_raw["Survived"]
 
-# male   = 1
-# female = 0
-train_data.ix[train_data.Sex == "male", "Sex"] = 1
-train_data.ix[train_data.Sex == "female", "Sex"] = 0
-train_data.Age = train_data.Age.fillna(train_data.Age.mean())
-
-
-# normalizing the data
+# scaling the data
 train_data_scaled = scale(train_data)
 
 # with gridsearch
@@ -52,20 +81,13 @@ print "gridsearch accuracy: " + str(accuracy_score(train_data_results, res1))
 print "regular accuracy: " + str(accuracy_score(train_data_results, res2))
 
 
-test_data = test_data_raw[["PassengerId", "Pclass", "Sex", "Age", "SibSp", "Parch", "Fare"]]
-# male   = 1
-# female = 0
-test_data.ix[test_data.Sex == "male", "Sex"] = 1
-test_data.ix[test_data.Sex == "female", "Sex"] = 0
-test_data.Age = test_data.Age.fillna(test_data.Age.mean())
-test_data.Fare = test_data["Fare"].fillna(test_data["Fare"].mean())
-# normalizing the data
-test_data_scaled = scale(test_data[["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare"]])
+# scaling the data
+test_data_scaled = scale(test_data)
 #predicting the outcome
 test_prediction = model2.predict(test_data_scaled)
 
 output = pd.DataFrame({
-    "PassengerId": test_data["PassengerId"],
+    "PassengerId": test_data_raw["PassengerId"],
     "Survived": test_prediction
     })
 output = output.set_index("PassengerId")
