d13eb39f32c1dc054a1580ee86d85b46dea745d8
viligabi <viligabi@gmail.com>
2016-06-25 11:05:25 +0200

    Add: check_cv(): cross validation score plotting function

--- titanic.py
+++ titanic.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.svm import LinearSVC
 from sklearn import svm
 from sklearn import cross_validation
@@ -8,6 +9,7 @@ from sklearn import tree
 from sklearn import grid_search
 from sklearn.preprocessing import scale
 import numpy as np
+import time
 
 def accuracy_score(truth, pred):
     """ Returns accuracy score for input truth and predictions. """
@@ -30,7 +32,7 @@ def optimize_data(df_):
     df.loc[df.Sex != 'male', 'Sex'] = 0
     df.loc[df.Sex == 'male', 'Sex'] = 1
     df.Age = df.Age.fillna(df.Age.mean())
-    df.Cabin.fillna('0', inplace=True)
+
     df.loc[df.Cabin.str[0] == 'A', 'Cabin'] = 1
     df.loc[df.Cabin.str[0] == 'B', 'Cabin'] = 2
     df.loc[df.Cabin.str[0] == 'C', 'Cabin'] = 3
@@ -38,7 +40,12 @@ def optimize_data(df_):
     df.loc[df.Cabin.str[0] == 'E', 'Cabin'] = 5
     df.loc[df.Cabin.str[0] == 'F', 'Cabin'] = 6
     df.loc[df.Cabin.str[0] == 'G', 'Cabin'] = 7
-    df.loc[df.Cabin.str[0] == 'T', 'Cabin'] = 8
+    try:
+        df.loc[df.Cabin.str[0] == 'T', 'Cabin'] = 8
+    except: pass
+    df.Cabin.fillna('0', inplace=True)
+
+
     df.Embarked.fillna(0, inplace=True)
     df.loc[df.Embarked == 'C', 'Embarked'] = 1
     df.loc[df.Embarked == 'Q', 'Embarked'] = 2
@@ -53,8 +60,9 @@ def optimize_data(df_):
     title = df.Name.str.replace("[^aA-zZ \w]", "").str.split().str[1]
     title = title.replace(['Mlle', 'Ms', "Miss"], 1) # 'Miss'
     title = title.replace(['Mme', 'Mrs'], 2) # Mrs
-    title = title.replace(['Rev', 'Dr', 'Master', 'Major', 'Col', 'Capt', 'Jonkheer', 'Dona'], 3) # Esteemed
-    title = title.replace(['Don', 'Lady', 'Sir', 'the Countess'], 4) # Royalty
+    title = title.replace(['Mr'], 3)  # Mrs
+    title = title.replace(['Rev', 'Dr', 'Master', 'Major', 'Col', 'Capt', 'Jonkheer', 'Dona'], 4) # Esteemed
+    title = title.replace(['Don', 'Lady', 'Sir', 'the Countess'], 5) # Royalty
     title = pd.to_numeric(title, errors='coerce').fillna(0)
     df.Name = title
 
@@ -65,12 +73,12 @@ def optimize_data(df_):
     df["ticket_ID"] = df.Ticket.str.replace("[^aA-zZ \w 0-9]", "").str.split().str[0].map(ticket_ID.get).fillna(0)
 
     # If data type is categorical, convert to dummy variables
-    df = df.join(pd.get_dummies(df["Pclass"], prefix="Pclass"))
+    #df = df.join(pd.get_dummies(df["Pclass"], prefix="Pclass"))
     df = df.join(pd.get_dummies(df["Cabin"], prefix="Cabin"))
     df = df.join(pd.get_dummies(df["Embarked"], prefix="Embarked"))
     df = df.join(pd.get_dummies(df["Fare"], prefix="Fare"))
     # removing the original columns
-    df.drop('Pclass', axis=1, inplace=True)
+    #df.drop('Pclass', axis=1, inplace=True)
     df.drop('Cabin', axis=1, inplace=True)
     df.drop('Embarked', axis=1, inplace=True)
     df.drop('Fare', axis=1, inplace=True)
@@ -100,6 +108,44 @@ def check_model(model, train_scaled, train_res):
     # print "model accuracy: " + str(accuracy_score(prediction, y_train))
     return model
 
+def check_cv(svc, X,y):
+    c_len = 10
+    gamm_len = 10
+    C_s = np.logspace(-1, 4, c_len)
+    gamma_s = np.logspace(-4, 1, gamm_len)
+    scores = np.zeros([c_len, gamm_len])
+    scores_std = np.zeros([c_len, gamm_len])
+
+    # Do the plotting
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(scores)
+    fig.colorbar(cax)
+    ax.set_xlabel("Gamma")
+    ax.set_ylabel("C")
+    plt.tight_layout()
+    i=0
+    j=0
+    from pylab import draw
+    for C in C_s:
+        j = 0
+        for gamma in gamma_s:
+            print "C: {}\t\t Gamma: {}\t".format(np.round(C,5),np.round(gamma,5)),
+            svc.C = C
+            svc.gamma = gamma
+            this_scores = cross_validation.cross_val_score(svc, X, y, n_jobs=1)
+            scores[i][j] = np.mean(this_scores)
+            scores_std[i][j] = np.std(this_scores)
+            print scores[i][j]
+            plt.clf()
+            cax = ax.matshow(scores)
+
+            j+=1
+        i+=1
+
+
+    pass
+
 # Import train and test raw data
 train_data_raw = pd.read_csv("train.csv")
 test_data_raw = pd.read_csv("test.csv")
@@ -109,10 +155,10 @@ test_data = optimize_data(test_data_raw)
 
 # Remove the features with low variance
 from sklearn.feature_selection import VarianceThreshold
-sel = VarianceThreshold(threshold=(.8 * (1 - .8)))
-sel.fit(train_data).variances_<(.8 * (1 - .8))
-train_data = train_data[train_data.columns[sel.fit(train_data).variances_>(.8 * (1 - .8))]]
-test_data = test_data[train_data.columns[sel.fit(train_data).variances_>(.8 * (1 - .8))]]
+P = .8
+sel = VarianceThreshold(threshold=(P * (1 - P)))
+train_data = train_data[train_data.columns[sel.fit(train_data).variances_>(P * (1 - P))]]
+test_data = test_data[train_data.columns[sel.fit(train_data).variances_>(P * (1 - P))]]
 
 # test_data must contain the same columns as train data due to model fitting and prediction
 for column in train_data.columns:
@@ -137,15 +183,10 @@ test_data_scaled = scale(test_data)
 # clf = grid_search.GridSearchCV(tree.DecisionTreeClassifier(), parameters,cv = 5)
 #model = clf.fit(train_data_scaled, train_data_results)
 # without gridsearch
-#model2 = tree.ExtraTreeClassifier().fit(train_data_scaled, train_data_results)
-from sklearn import tree
-from sklearn.linear_model import SGDClassifier
-
+check_cv(svm.SVC(), train_data_scaled, train_data_results)
 model = check_model(svm.SVC(), train_data_scaled, train_data_results)
-#model = check_model(tree.DecisionTreeClassifier(), train_data_scaled, train_data_results)
-#model = check_model(tree.ExtraTreeClassifier(), train_data_scaled, train_data_results)
-#model = check_model(SGDClassifier(), train_data_scaled, train_data_results)
-
+from sklearn.learning_curve import learning_curve
+a, on_train, on_test= learning_curve(svm.SVC(), train_data_scaled, train_data_results)
 #predicting the outcome
 test_prediction = model.predict(test_data_scaled)
 
